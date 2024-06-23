92c7aa196b0604056c88754f3f192b65bf836ace
Julien Meyer <julienmeyer32@gmail.com>
2024-06-23 21:05:36 +0200

    :sparkles: feat: project structure

---
+++ presentation/src/main/java/fr/epita/discover/SpaceYX.java
@@ -0,0 +1,17 @@
+package fr.epita.discover;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lummix (julienmeyer32@gmail.com)
+ * @project Default (Template) Project
+ * 23/06/2024
+ */
+@SpringBootApplication(scanBasePackages = {
+        "fr.epita.discover",
+})
+public class SpaceYX {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
